b7d78909f66d0a3382f481dd05d10ebae8f7fe51
zinthi1a <zinthiag@gmail.com>
2026-04-28 20:33:52 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// All routes serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Undertone SKN running on port ${PORT}`);
+});
